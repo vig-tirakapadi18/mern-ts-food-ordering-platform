db2d30ef0dfeb73c33750746ca3f887d9e9cb6b5
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2024-07-14 09:25:01 +0530

    created User model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+module.exports = UserModel;
