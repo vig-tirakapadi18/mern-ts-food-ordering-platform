7e1b602798647f09e96e6ec3a814b954e188cd34
Vighnesh Tirakapadi <vighnesh.tirakapadi@deeporion.com>
2024-07-15 12:03:06 +0530

    added react-query to make api request

--- client/src/auth/Auth0ProviderWithNavigate.tsx
+++ client/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/user.api";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import { ReactNode } from "react";
 
@@ -8,6 +9,8 @@ interface Auth0ProviderWithNavigateProps {
 const Auth0ProviderWithNavigate = ({
   children,
 }: Auth0ProviderWithNavigateProps) => {
+  const { createUser } = useCreateMyUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
@@ -16,6 +19,9 @@ const Auth0ProviderWithNavigate = ({
     throw new Error("Unable to initialize AUTH!");
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
     console.log(appState, user);
   };
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import App from "./App.tsx";
 import "./global.css";
 import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <App />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
